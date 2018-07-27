983f21da8d63d64846de9a66b553bc08ece4039e
Kevin Alejandro <37127939+TheCodeBite@users.noreply.github.com>
2018-07-27 14:28:03 -0500

    algo
    algo

---
+++ algo.php
@@ -0,0 +1,7 @@
+Route::post("registrar", "UsuarioController@registrarUsuario");
+
+Route::get():
+
+
+//vamos a la carpeta app /controller/creamos nuevo controlador (mismonombre que se le dio tiene que llevar controller)
+
---
+++ prueba.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>prueba</title>
+</head>
+<body>
+	<h1>SI FUNCIONA</h1>
+	
+
+</body>
+</html>
