c0144f725374c6be06ed1c67d0f893b6158bf1d5
Daniel-Sarmiento <35851699+Daniel-Sarmiento@users.noreply.github.com>
2018-07-30 07:29:45 -0500

    Delete algo.php

--- algo.php
+++
@@ -1,7 +0,0 @@
-Route::post("registrar", "UsuarioController@registrarUsuario");
-
-Route::get():
-
-
-//vamos a la carpeta app /controller/creamos nuevo controlador (mismonombre que se le dio tiene que llevar controller)
-
